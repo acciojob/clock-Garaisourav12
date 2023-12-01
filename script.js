d8de40d6c49f3f22505b82e76adce85510232880
SOURAV GARAI <101336956+Garaisourav12@users.noreply.github.com>
2023-12-01 12:32:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 //your JS code here. If required.
+
+    function updateTimer() {
+      const timerElement = document.getElementById('timer');
+      const now = new Date();
+
+      const options = {
+        month: 'numeric',
+        day: 'numeric',
+        year: 'numeric',
+        hour: 'numeric',
+        minute: 'numeric',
+        second: 'numeric',
+      };
+
+      const formattedDate = now.toLocaleString('en-US', options);
+      timerElement.textContent = formattedDate;
+    }
+
+    // Update the timer every second
+    setInterval(updateTimer, 1000);
+
+    // Initial call to set the timer immediately
+    updateTimer();
